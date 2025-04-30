358c1d44b5bb1b200a6e35c69a046b0c08fae3fa
Mauro Rosero P. <mauro@rosero.one>
2025-04-30 06:30:08 -0500

    [STYLE] Add shebang line to header_manage.py for improved script execution

--- header_manage.py
+++ header_manage.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+ #!/usr/bin/env python3
 # -----------------------------------------------------------------------------
 # Copyright (c) 2025, MAURO ROSERO PÉREZ
 # License: GPLV3
