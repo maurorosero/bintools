7f884aa3a9f9660be21d54d944a042e7b57f072c
Mauro Rosero P. <mauro@rosero.one>
2025-06-15 12:32:11 -0500

    [FIX] Corrección definitiva a la actualización automática de la Fecha de Modificación

--- .githooks/quality_manager.py
+++ .githooks/quality_manager.py
@@ -9,7 +9,7 @@ Script Name: quality_manager.py
 Version:     0.1.1
 Description: Gestiona los niveles de calidad y formatos de commit de manera independiente.
 Created:     2025-06-14
-Modified:    2025-06-15 12:30:59
+Modified:    2025-06-15 12:32:13
 Author:      Mauro Rosero Pérez <mauro@rosero.one>
 Assistant:   Cursor AI (https://cursor.com)
 """
